04c7898cd3447ee3984dd5a400cab5e15816c2ac
antomys <ihor.volokhovych@unicyb.kiev.ua>
2021-04-11 00:51:42 +0300

    Implemented Lzw. Needed to multi thread and work with memory issues

--- Lzw.Demo/Program.cs
+++ Lzw.Demo/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.IO;
+using Lzw.Demo.Compression;
 
 namespace Lzw.Demo
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var compressor = new LzwCompression();
+
+            using var fileStream = new FileStream("1.txt", FileMode.Open, FileAccess.Read);
+            using var binaryReader = new BinaryReader(fileStream);
+            using var createFile = new FileStream("1.lzw", FileMode.CreateNew);
+            using var binaryWriter = new BinaryWriter(createFile);
+
+            //todo: use threads or tasks
+            compressor.Compress(binaryReader, binaryWriter);
+            //compressor.Decompress(binaryReader, binaryWriter);
+
         }
     }
 }
